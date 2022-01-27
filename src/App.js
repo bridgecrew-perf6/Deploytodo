400ccb3f3b43d8b9be7e94c9e9f8d819e659b8b6
nechiyan <navasnechiyan3@gmail.com>
2022-01-27 15:51:48 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import "./App.css";
+import "./components/screens/ToDo"
+import ToDo from './components/screens/ToDo';
+export class App extends Component {
+  render() {
+    return <ToDo />
+  }
+}
+
+export default App;
